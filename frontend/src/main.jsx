6dc9e19703641315eeebae240cb8699ef0dae514
salmunandoni11 <salmun.andoni.11@gmail.com>
2026-08-28 18:55:58 +0700

    add comment for test merge

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,5 +2,5 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
-
+/* eslint-disable-next-line no-undef */
 createRoot(document.getElementById('root')).render(<StrictMode><App /></StrictMode>)
